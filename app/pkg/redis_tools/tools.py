2fcabc5930a232de269ca42b70240c0245d2c5df
gresaggr <sergeyzhirenkin@gmail.com>
2024-03-18 11:28:30 +0300

    fix annotation type

--- app/pkg/redis_tools/tools.py
+++ app/pkg/redis_tools/tools.py
@@ -6,10 +6,23 @@ class RedisTools:
 
     @classmethod
     def set_pair(cls, pair: str, price: str):
+        """
+        Set the pair and price in the Redis connection.
+
+        :param pair: str - The pair to set the price for.
+        :param price: str - The price to set for the pair.
+        :return: None
+        """
         cls.__redis_connect.set(pair, price)
 
     @classmethod
     def get_pair(cls, pair):
+        """
+        Get the value for a given pair in the Redis cache.
+
+        :param pair: The key to retrieve the value for.
+        :return: The value corresponding to the key in the Redis cache.
+        """
         return cls.__redis_connect.get(pair)
 
     @classmethod
